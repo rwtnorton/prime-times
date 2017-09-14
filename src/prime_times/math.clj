3b19188458f5a706b7ecd2409601c26df50dca06
Richard W. Norton <rwtnorton@gmail.com>
2017-09-13 21:50:08 -0700

    refine valid inputs of prime? predicate

--- src/prime_times/math.clj
+++ src/prime_times/math.clj
@@ -3,24 +3,12 @@
 (defn prime?
   "Returns true iff given number is prime."
   [n]
-  (let [m (->> n Math/sqrt long)
-        multiple? (fn [x]
-                    (zero? (mod n x)))]
-    (if (< n 2)
-      false
-      (->> (range 2 (inc m))
-           (not-any? multiple?))))
-
-  #_(let [f (fn [num-primes]
-            (let [prime?
-                  (fn [n]
-                    (let [m (->> n Math/sqrt long)
-                          multiple? (fn [x]
-                                      (zero? (mod n x)))]
-                      (if (< n 2)
-                        false
-                        (->> (range 2 (inc m))
-                             (not-any? multiple?)))))]
-              (take num-primes (filter prime? (range)))))]
-    (is (all? (p67 f)))))
-
+  (if (integer? n)
+    (let [m (->> n Math/sqrt long)
+          multiple? (fn [x]
+                      (zero? (mod n x)))]
+      (if (< n 2)
+        false
+        (->> (range 2 (inc m))
+             (not-any? multiple?))))
+    false))
--- test/prime_times/math_test.clj
+++ test/prime_times/math_test.clj
@@ -2,15 +2,15 @@
   (:require [clojure.test :refer :all]
             [prime-times.math :as m]))
 
+(def not-prime? (complement m/prime?))
+
 (deftest prime?
   (testing "prime numbers"
     (are [n] (m/prime? n)
-      2 3 5 7 11 541))
+      2 3 5 7 11 541 29N))
   (testing "composite numbers"
-    (are [n] ((complement m/prime?) n)
-      1 4 6 8 10 999)))
-
-#_(defn p67 [__]
-  [(= (__ 2) [2 3])
-   (= (__ 5) [2 3 5 7 11])
-   (= (last (__ 100)) 541)])
+    (are [n] (not-prime? n)
+      1 4 6 8 10 999))
+  (testing "wacky values are not prime"
+    (are [n] (not-prime? n)
+      -1 0 nil "" "42" :so-prime false Math/E 99M)))
