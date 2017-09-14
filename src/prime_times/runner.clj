a64bbc54bc6cfd9e62b83cca34149ee5e247da2b
Richard W. Norton <rwtnorton@gmail.com>
2017-09-14 01:15:15 -0700

    support cli options

--- src/prime_times/runner.clj
+++ src/prime_times/runner.clj
@@ -1,8 +1,23 @@
 (ns prime-times.runner
   (:require [prime-times.math :as math]
-            [prime-times.tabular :as tabs]))
+            [prime-times.tabular :as tabs]
+            [clojure.tools.cli :as cli]))
 
 (defn -main
   [& args]
-  (let [n 10]
-    (print (tabs/tablify (math/n-primes n)))))
+  (let [[opts args banner]
+        (cli/cli args
+                 ["-h" "--help" "Print this help"
+                  :flag true
+                  :default false]
+                 ["-n" "--count" "Number of primes"
+                  :default 10
+                  :parse-fn (fn [s] (Long/parseLong s))
+                  :validate [(fn [s]
+                               (re-seq #"^\d+$" s))
+                             "Must be a non-negative whole number"]])]
+    (when (:help opts)
+      (println banner)
+      (System/exit 0))
+
+    (print (tabs/tablify (math/n-primes (:count opts))))))
