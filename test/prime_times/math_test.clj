894b6c6121d33d8ff3b2850891bba16b9545119d
Richard W. Norton <rwtnorton@gmail.com>
2017-09-13 21:36:01 -0700

    test that composite numbers are not prime

--- test/prime_times/math_test.clj
+++ test/prime_times/math_test.clj
@@ -5,7 +5,10 @@
 (deftest prime?
   (testing "prime numbers"
     (are [n] (m/prime? n)
-      2 3 5 7 11)))
+      2 3 5 7 11 541))
+  (testing "composite numbers"
+    (are [n] ((complement m/prime?) n)
+      1 4 6 8 10 999)))
 
 #_(defn p67 [__]
   [(= (__ 2) [2 3])
